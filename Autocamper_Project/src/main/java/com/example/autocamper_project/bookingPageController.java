b27e5a03e21886af7ab9b74723f1f2e9dddea674
Ronnie <148753940+Nessumsa@users.noreply.github.com>
2024-04-23 11:34:07 +0200

    Various fxml views

---
+++ Autocamper_Project/src/main/java/com/example/autocamper_project/bookingPageController.java
@@ -0,0 +1,79 @@
+package com.example.autocamper_project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class bookingPageController {
+
+    @FXML
+    private TextField orderNumber;
+    @FXML
+    private TextField customerID;
+    @FXML
+    private TextField phoneNum;
+    @FXML
+    private Button loadCustomerData;
+    @FXML
+    private TextField employeeID;
+    @FXML
+    private Label discountRate;
+    @FXML
+    private TextField fullName;
+    @FXML
+    private TextField adress;
+    @FXML
+    private TextField zipcode;
+    @FXML
+    private TextField city;
+    @FXML
+    private TextField country;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField driverLicenseNum;
+    @FXML
+    private DatePicker dateOfIssue;
+    @FXML
+    private Button createCustomer;
+    @FXML
+    private ListView listOfDrivers;
+    @FXML
+    private DatePicker startDate;
+    @FXML
+    private DatePicker endDate;
+    @FXML
+    private ListView listOfAutocampers;
+    @FXML
+    private ComboBox chooseInsurance;
+    @FXML
+    private Button calculatePrice;
+    @FXML
+    private Label totalPrice;
+    @FXML
+    private Button confirmBooking;
+    @FXML
+    private Button cancelBooking;
+    @FXML
+    private void onLoadCustomerDataButtonClick(){
+
+    }
+    @FXML
+    private void onCreateCustomerButtonClick(){
+
+    }
+    @FXML
+    private void onCalculatePriceButtonClick(){
+
+    }
+    @FXML
+    private void onConfirmBookingButtonClick(){
+
+    }
+    @FXML
+    private void onCancelBookingButtonClick(){
+
+    }
+
+
+
+}
--- Autocamper_Project/src/main/java/com/example/autocamper_project/mainViewController.java
+++ Autocamper_Project/src/main/java/com/example/autocamper_project/mainViewController.java
@@ -3,12 +3,12 @@ package com.example.autocamper_project;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class Controller {
+public class mainViewController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onNewBookingButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
